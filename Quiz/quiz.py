f598a09d5d6f195dd43b7326de2ab5a22a8e8741
Natan8963 <natan8963@gmail.com>
2023-05-03 16:10:03 -0300

    first commit

---
+++ Quiz/quiz.py
@@ -0,0 +1,48 @@
+print("Seja muito bem vindo ao quiz do Iury!")
+answer_user = input("Quer começar? (S/N) ")
+
+if answer_user != "S":
+    quit()
+
+score = 0
+
+print("Começando...")
+print("Quem desenvolveu o jogo Grand Theft Auto (GTA)? \n (A) Rockstar Games \n (B) Ubisoft \n (C) Activision \n (D) EA \n")
+answer_1 = input("Resposta: ")
+
+if answer_1 == "A":
+    print("Correto!")
+    score = score + 1
+else:
+    print("Incorreto!")
+
+print("Qual o nome do protagonista do jogo GTA San Andreas?\n (A) Carlos John \n (B) Carl Jonhson \n (C) Carl Jaqueline \n (D) Carlos Jonhson \n")
+answer_2 = input("Resposta: ")
+
+if answer_2 == "B":
+    print("Correto!")
+    score = score + 1
+else:
+    print("Incorreto!")
+
+print("Qual o nome do protagonista do jogo The las of Us Parte 2?\n (A) Joel Miller \n (B) Carl Jonhson \n (C) Ellie Williams \n (D) Arthur Morgam \n")
+answer_3 = input("Resposta: ")
+
+if answer_3 == "C":
+    print("Correto!")
+    score = score + 1
+else:
+    print("Incorreto!")
+
+
+
+
+
+print(f"Quiz acabou... Pontuação: {score}/3")
+
+
+
+
+
+
+
